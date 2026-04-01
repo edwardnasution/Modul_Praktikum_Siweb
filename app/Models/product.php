9f75bfde3aa1dd7d56e19597fa03298f60fbc3a2
Radik Alfisyar <radikalfisyar@gmail.com>
2026-04-01 17:42:03 +0700

    Menambahkan koneksi ke database  (MVC)

---
+++ app/Models/product.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class product extends Model
+{
+    protected $table = 'products';
+    protected $primaryKey = 'product_id';
+    public $timestamps = false; // Aktifkan timestamps
+    protected $fillable = ['product_id', 'category_id', 'product_name', 'product_price', 'product_stock'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'category_id', 'category_id');
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,48 +1,35 @@
 <!doctype html>
-<html lang="en">
+<html lang="id">
 
 <head>
   <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sistem Manajemen Sepatu</title>
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
-    rel="stylesheet" />
 
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
   <link rel="stylesheet" href="{{ asset('css/style.css') }}" />
 </head>
 
 <body>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-      <a class="navbar-brand" href="{{ route('home') }}">CIBADUYUT SHOES</a>
-      
-      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-        @if(session()->has('user'))
-          <span class="text-white me-3">
-             Halo, {{ session('user') }}
-          </span>
-          <a href="{{ route('logout') }}" class="btn btn-danger btn-sm" onclick="return confirm('Yakin ingin keluar?')">
-            Logout
-          </a>
-        @else
-          <a href="{{ route('login') }}" class="btn btn-warning btn-sm">
-            Login
-          </a>
-        @endif
-      </div>
 
-      <div class="d-flex align-items-center ms-auto gap-2 ps-3">
-        <button
-          class="btn btn-outline-warning btn-sm"
-          data-bs-toggle="modal"
-          data-bs-target="#wishlistModal">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container">
+      <a class="navbar-brand" href="#">CIBADUYUT SHOES</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        @if(session('user'))
+        <span class="text-white me-3">
+          {{ session('user')}}
+        </span>
+        <a href="{{ route('logout') }}" class="btn btn-danger btn-sm">Logout</a>
+        @else
+        <a href="{{ route('login') }}" class="btn btn-warning btn-sm">Login</a>
+        @endif
+        <button id="btn-wishlist-nav" class="btn btn-outline-warning btn-sm me-2" data-bs-toggle="modal"
+          data-bs-target="#whislistModal">
           ⭐ Wishlist (<span id="wishlist-count">0</span>)
-        </button>
-        <button
-          class="btn btn-outline-success btn-sm"
-          data-bs-toggle="modal"
-          data-bs-target="#cartModal">
-          🛒 Keranjang (<span id="cart-count">0</span>)
         </button>
         <button id="btn-theme" class="btn btn-outline-light btn-sm">
           Mode Gelap
@@ -54,13 +41,13 @@
   <div class="hero text-center text-white d-flex align-items-center">
     <div class="container">
       <h1>Sistem Manajemen Sepatu</h1>
-      <p>Kelola Data Sepatu Dengan Mudah</p>
+      <p>Kelola Data sepatu dengan Mudah</p>
     </div>
   </div>
 
   <div class="container mt-5">
     <div class="row text-center">
-      <div class="col-md-4">
+      <div class="col-md-4 mb-3">
         <div class="card dashboard-card">
           <div class="card-body">
             <h5>Total Produk</h5>
@@ -68,7 +55,7 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4 mb-3">
         <div class="card dashboard-card">
           <div class="card-body">
             <h5>Stok Tersedia</h5>
@@ -76,7 +63,7 @@
           </div>
         </div>
       </div>
-      <div class="col-md-4">
+      <div class="col-md-4 mb-3">
         <div class="card dashboard-card">
           <div class="card-body">
             <h5>Kategori</h5>
@@ -87,62 +74,66 @@
     </div>
   </div>
 
-  <div class="container mt-5">
+  <div class="d-flex justify-content-between mb-3">
     <h3 class="mb-4">Daftar Sepatu</h3>
-    <div class="row" id="container-barang">
-      
-      <div class="col-md-4 mb-4">
-        <div class="card h-100">
-          <img src="{{ asset('assets/NIKE_P_6000.jpg') }}" class="card-img-top" alt="Sepatu" />
-          <div class="card-body">
-            <h5 class="card-title">Nike P-6000</h5>
-            <p class="card-text harga-text">Harga: Rp 1.429.000</p>
-            <p class="card-text stok-text">Stok: 10</p>
-            <div class="d-flex justify-content-between">
-              <button class="btn btn-primary btn-detail w-50 me-2">Beli</button>
-              <button class="btn btn-outline-danger btn-wishlist w-50">❤ Wishlist</button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <a href="{{ route('products') }}" class="text-decoration-none text-black mb-3">
+      Lihat Semua Produk >>>
+    </a>
+  </div>
 
-      <div class="col-md-4 mb-4">
-        <div class="card h-100">
-          <img src="{{ asset('assets/AIR_FORCE_1.jpg') }}" class="card-img-top" alt="Sepatu" />
-          <div class="card-body">
-            <h5 class="card-title">Nike Air Force 1</h5>
-            <p class="card-text harga-text">Harga: Rp 1.259.000</p>
-            <p class="card-text stok-text">Stok: 7</p>
-            <div class="d-flex justify-content-between">
-              <button class="btn btn-primary btn-detail w-50 me-2">Beli</button>
-              <button class="btn btn-outline-danger btn-wishlist w-50">❤ Wishlist</button>
-            </div>
-          </div>
+  <div class="modal fade" id="whislistModal" tabindex="-1" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title">Daftar Wishlist Saya</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-      </div>
-
-      <div class="col-md-4 mb-4">
-        <div class="card h-100">
-          <img src="{{ asset('assets/AIR_JORDAN_1_LOW.jpg') }}" class="card-img-top" alt="Sepatu" />
-          <div class="card-body">
-            <h5 class="card-title">Nike Air Jordan 1 Low</h5>
-            <p class="card-text harga-text">Harga: Rp 1.729.000</p>
-            <p class="card-text stok-text">Stok: 10</p>
-            <div class="d-flex justify-content-between">
-              <button class="btn btn-primary btn-detail w-50 me-2">Beli</button>
-              <button class="btn btn-outline-danger btn-wishlist w-50">❤ Wishlist</button>
-            </div>
-          </div>
+        <div class="modal-body">
+          <ul class="list-group" id="daftar-items">
+          </ul>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
+          <button type="button" class="btn btn-danger" onclick="hapusWishlist()">Kosongkan</button>
         </div>
       </div>
     </div>
   </div>
 
-  <footer class="bg-dark text-white text-center p-3 mt-5">
-    &copy; 2026 Sistem Manajemen Sepatu Toko Sepatu.
+  <div class="container mt-5 mb-5">
+    <h3 class="mb-4">Tambah Sepatu</h3>
+    <div class="card p-4">
+      <form id="form-tambah">
+        <div class="mb-3">
+          <label class="form-label">Nama Sepatu</label>
+          <input type="text" class="form-control" placeholder="Masukkan nama sepatu" required />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Harga</label>
+          <input type="number" class="form-control" placeholder="Masukkan harga" required />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Stok</label>
+          <input type="number" class="form-control" placeholder="Masukkan stok" required />
+        </div>
+        <div class="mb-3">
+          <label class="form-label">Kategori</label>
+          <select class="form-select">
+            <option>Running</option>
+            <option>Basket</option>
+            <option>Casual</option>
+          </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+      </form>
+    </div>
+  </div>
+
+  <footer class="bg-dark text-white text-center p-3">
+    © 2026 Sistem Manajemen Sepatu
   </footer>
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="{{ asset('js/script.js') }}"></script>
 </body>
 
